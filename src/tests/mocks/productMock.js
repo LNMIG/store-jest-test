151dc6910f04459b0e0bc213cc0f2fe8f4ee39d5
LNMIG <migliorenicolas@yahoo.com.ar>
2022-11-05 17:31:59 -0300

    MOCKS: some mocks created

---
+++ src/tests/mocks/productMock.js
@@ -0,0 +1,10 @@
+import imageId1 from '../../assets/Tshirt-Argentina.jpg'
+
+const ProductMock = {
+    'id': '1',
+    'image': imageId1,
+    'title': 'Camiseta',
+    'price': 25,
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+}
+export default ProductMock
---
+++ src/tests/mocks/providerMock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { createBrowserHistory } from 'history';
+import reducer from '../../reducers/index.js';
+
+const store = configureStore({ reducer });
+const history = createBrowserHistory()
+
+const ProviderMock = props => {
+    <Provider store={store}>
+        <BrowserRouter history={history}>
+            {props.children}
+        </BrowserRouter>
+    </Provider>
+}
+export default ProviderMock
