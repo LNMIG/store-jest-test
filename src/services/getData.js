538f04dfeaa4871de38fd629257f9c60563f9117
LNMIG <migliorenicolas@yahoo.com.ar>
2022-11-05 19:56:29 -0300

    DOCS: fake function for fetching created to be tested

---
+++ src/services/getData.js
@@ -0,0 +1,10 @@
+const getData = async (api) => {
+    try {
+        const response = await fetch(api)
+        const response_1 = await response.json()
+        return response_1
+    } catch (error) {
+        return error
+    }
+}
+export default getData
